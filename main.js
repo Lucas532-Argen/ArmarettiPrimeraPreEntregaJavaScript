8f48a7685c4e845c60f2d3a0f90c28e599170557
Lucas Armaretti <larmaretti@gmail.com>
2023-07-09 19:01:58 -0300

    generando dinamica del sitio

--- main.js
+++ main.js
@@ -1,37 +1,63 @@
-var Comprobante_de_domicilio = false
-let Presenta_alguna_enfermedad = false
-let Informe_Medico = false
+let mostrador = document.getElementById("mostrador");
+let seleccion = document.getElementById("seleccion");
+let imgSeleccionada = document.getElementById("img");
+let modeloSeleccionado = document.getElementById("modelo");
+let descripSeleccionada = document.getElementById("descripcion");
+let precioSeleccionado = document.getElementById("precio");
 
-function calcularPlan() {
-  var edad = parseInt(prompt("Cual es tu edad?"));
-  console.log("Tu edad es " + edad)
+function cargar(item){
+    quitarBordes();
+    mostrador.style.width = "60%";
+    seleccion.style.width = "40%";
+    seleccion.style.opacity = "1";
+    item.style.border = "2px solid red";
 
-  var domicilio = parseInt(prompt("domicilio 1 o 2?"));
+    imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
 
-  var enfermedad = parseInt(prompt("enfermedad 3 o 4"));
+    modeloSeleccionado.innerHTML =  item.getElementsByTagName("p")[0].innerHTML;
 
-  var informe = parseInt(prompt("informe 5 o 6"));
+    descripSeleccionada.innerHTML = "Descripción del modelo ";
 
-  if (domicilio === 1) {
-    while (edad > 15) {
-      if (edad > 60 && informe === 5) {
-        alert("Tu Plan es + Avanzado")
-        break
-      }
-      if (edad >= 15 && edad <= 40 && informe === 5) {
-        alert("Tu plan es Intermedio")
-        break
-      }
-      if (edad >= 10 && edad <= 100 && enfermedad === 3) {
-        alert("Tu plan es Avanzado")
-        break;
-      }
+    precioSeleccionado.innerHTML =  item.getElementsByTagName("span")[0].innerHTML;
 
+
+}
+function cerrar(){
+    mostrador.style.width = "100%";
+    seleccion.style.width = "0%";
+    seleccion.style.opacity = "0";
+    quitarBordes();
+}
+function quitarBordes(){
+    var items = document.getElementsByClassName("item");
+    for(i=0;i <items.length; i++){
+        items[i].style.border = "none";
     }
-  } else {
-    alert("Debe ingresar Domicilio")
-  }
 }
 
 
 
+
+
+// const productos = [
+//     { nombre: "NIKE AIR 97", precio: 1300 },
+//     { nombre: "NIKE RUNNING TERRA ", precio: 1800 },
+//     { nombre: "NIKE WINFLO 8NIKE WINFLO 8", precio: 3600 },
+//     { nombre: "NIKE LOW-TOP", precio: 1800 },
+//     { nombre: "NIKE BLAZER 97", precio: 3000 },
+//     { nombre: "NIKE LEGEND ESENTIAL", precio: 2000 },
+//     { nombre: "NIKE AIR ZOOM", precio: 2500 },
+//     { nombre: "NIKE TERRA KING", precio: 1800 },
+// ];
+
+
+//   alert("Bienvenidos a NIKE SHOES! Crean tu Cuenta!")
+//   let email = String(prompt("Ingrese su E-mail"))
+//   let password = String(prompt("Ingrese Contraseña"))
+//   alert("Cuenta Creada, con exito")
+
+
+
+
+//let productoElegido = prompt("Ingrese el producto que quiere comprar");
+//console.log( productos.find((producto) => producto.nombre === productoElegido ) );
